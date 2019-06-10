75f41e4560af74775a0cfecd3d272e2cfa74ca9f
George Bougakov <githubinq@bygeorgenet.me>
2019-06-10 17:50:29 +0300

    Fix build

--- start-client.js
+++ start-client.js
@@ -6,14 +6,14 @@ const app = express()
 app.use('/public', express.static('public'))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'host.html')
+  res.sendFile(path.join(__dirname, 'public', 'host.html'))
 })
 
 app.get('/l/:base64String', (req, res) => {
   const base64Regex = /(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)/
   const base64String = req.params.base64String
   if (base64Regex.test(base64String)) {
-    return res.sendFile(path.join(__dirname, 'public', 'guest.html')
+    return res.sendFile(path.join(__dirname, 'public', 'guest.html'))
   }
   res.status(404).send('Not Found')
 })
