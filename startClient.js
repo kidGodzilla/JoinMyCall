586e7a659ec75a42cfe439d6d0fb7e43e3c6d040
Kumar Abhirup <kumar@iqubex.com>
2019-06-10 19:55:28 +0530

    📦 NEW: Local dev environment!

--- startClient.js
+++ startClient.js
@@ -14,4 +14,10 @@ app.get('/', (req, res, next) => {
   res.sendFile(__dirname + '/public/host.html')
 })
 
+app.get('/l/:base64String', (req, res, next) => {
+  const base64Regex = /(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)/
+  const base64String = req.params.base64String
+  base64Regex.test(base64String) ? res.sendFile(__dirname + '/public/guest.html') : res.sendStatus(404)
+})
+
 server.listen(process.env.PORT || 3001)
