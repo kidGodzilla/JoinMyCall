9f92814bc04ce4938d6e056e089df78a49f51796
Kumar Abhirup <kumar@iqubex.com>
2019-06-10 19:45:47 +0530

    👌 IMPROVE: Get dev server working

--- startClient.js
+++ startClient.js
@@ -1,1 +1,17 @@
-// lol
+const http = require('http')
+const express = require('express')
+
+const app = express()
+
+const server = http.createServer(app)
+
+app.use('/public/guest.js', express.static(__dirname + '/public/guest.js'))
+app.use('/public/host.js', express.static(__dirname + '/public/host.js'))
+app.use('/public/main.css', express.static(__dirname + '/public/main.css'))
+app.use('/public/logo.svg', express.static(__dirname + '/public/logo.svg'))
+
+app.get('/', (req, res, next) => {
+  res.sendFile(__dirname + '/public/host.html')
+})
+
+server.listen(process.env.PORT || 3001)
