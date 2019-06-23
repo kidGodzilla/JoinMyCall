de75325e6a96d6b5e29f67159b2b3f71f281b2f4
Kumar Abhirup <kumar@iqubex.com>
2019-06-23 12:59:51 +0530

    🐛 FIX: obey xo tests

---
+++ public/dark-mode.js
@@ -0,0 +1,13 @@
+/* global Darkmode */
+const options = {
+  bottom: '64px',
+  time: '0.5s',
+  mixColor: '#fff',
+  backgroundColor: '#fff',
+  buttonColorDark: '#100f2c',
+  buttonColorLight: '#fff',
+  label: '🌓'
+}
+
+const darkMode = new Darkmode(options)
+darkMode.showWidget()
