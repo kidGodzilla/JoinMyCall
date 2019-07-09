ec042162f09bfc4bf42ebfaeb5ca2b9802d45ea5
Kumar Abhirup <kumar@iqubex.com>
2019-07-09 20:39:35 +0530

    👌 IMPROVE: Respond to copy button

--- public/host.js
+++ public/host.js
@@ -1,19 +1,20 @@
 /* global Vue, btoa, ClipboardJS */
 /* eslint-disable no-new */
 new Vue({
-  /* eslint-enable no-new */
   el: '#app',
   data: {
     generatedLink: '',
-    enteredLink: ''
+    enteredLink: '',
+    copyStatus: 'Copy'
   },
   methods: {
     genLink() {
+      this.copyStatus = 'Copy' // Set status to default after every new generated link
+      let clipboard = new ClipboardJS('.copy-btn')
       this.generatedLink = btoa(this.enteredLink)
+      clipboard.on('success', () => {
+        this.copyStatus = 'Copied'
+      })
     }
   }
 })
-
-/* eslint-disable no-new */
-new ClipboardJS('.copy-btn')
-/* eslint-enable no-new */
